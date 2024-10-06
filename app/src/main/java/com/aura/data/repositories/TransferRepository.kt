0711e189147b2a2d8597dffc8db0d1e16c88f788
Maxime Girard <maxime.girard.cpro@gmail.com>
2024-10-06 22:23:13 +0200

    feat: ajout d'un delais pour le chargement des données

--- app/src/main/java/com/aura/data/repositories/TransferRepository.kt
+++ app/src/main/java/com/aura/data/repositories/TransferRepository.kt
@@ -3,10 +3,13 @@ package com.aura.data.repositories
 import com.aura.data.remote.ApiService
 import com.aura.data.remote.LoginRequest
 import com.aura.data.remote.TransferRequest
+import kotlinx.coroutines.delay
 
 class TransferRepository(private val api: ApiService) {
     suspend fun transferFunds(sender: String, recipient: String, amount: Double): Boolean {
+        delay(1000)
         val transfer = api.transfer(TransferRequest(sender, recipient, amount))
         return transfer.result
     }
 }
+
